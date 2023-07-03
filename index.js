502a7ce97ce1dbf622f5b57257655e371eef0441
paresh Sahoo <pareshrockyphry20002016@gmail.com>
2023-07-04 00:04:35 +0530

    Added JQUERY,song,animation

---
+++ index.js
@@ -0,0 +1,6 @@
+  // $(document).on("click", () => {
+  //   console.log("audio is playing");
+  //   const audio = $("#my_audio");
+  //   // audio.volume = 0.2;
+  //   audio.trigger('play');
+  // });
